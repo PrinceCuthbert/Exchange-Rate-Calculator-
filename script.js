ef237c3c91c16014ba125c934a29f419cc6c0a3b
ISHIMWE Prince Cuthbert <ishimweprincecuthbert@gmail.com>
2025-03-10 17:08:55 +0200

    second commit

--- script.js
+++ script.js
@@ -11,12 +11,13 @@ function calculate() {
   const currency_one = currencyEl_one.value;
   const currency_two = currencyEl_two.value;
 
-  fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+  // Replace with your API key from ExchangeRate-API
+  const apiKey = "440383da53a0387f7270622a";
+
+  fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/latest/${currency_one}`)
     .then((res) => res.json())
     .then((data) => {
-      // console.log(data);
-      const rate = data.rates[currency_two];
-
+      const rate = data.conversion_rates[currency_two];
       rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
       amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
     });
